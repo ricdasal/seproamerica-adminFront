9457b66a31cb333b64a61f6a1287cbc226959478
Ricardo David Salazar Secaira <ricdasal@espol.edu.ec>
2023-08-16 11:45:53 -0500

    Update crear-cargos.component.spec.ts

--- adminWeb/admin-in/src/app/configuraciones-wind/crear-cargos/crear-cargos.component.spec.ts
+++ adminWeb/admin-in/src/app/configuraciones-wind/crear-cargos/crear-cargos.component.spec.ts
@@ -2,22 +2,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CrearCargosComponent } from './crear-cargos.component';
 
-// describe('CrearCargosComponent', () => {
-//   let component: CrearCargosComponent;
-//   let fixture: ComponentFixture<CrearCargosComponent>;
+describe('CrearCargosComponent', () => {
+  let component: CrearCargosComponent;
+  let fixture: ComponentFixture<CrearCargosComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ CrearCargosComponent ]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CrearCargosComponent ]
+    })
+    .compileComponents();
 
-//     fixture = TestBed.createComponent(CrearCargosComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(CrearCargosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
